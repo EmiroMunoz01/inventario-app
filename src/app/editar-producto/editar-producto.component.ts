ec5f3ea3204d17f20b65a41d3aa2b1c361c7bd04
EmiroMunoz01 <emiro_2001@icloud.com>
2024-11-12 21:35:59 -0500

    Se ha creado el formulario de editar en la vista

--- src/app/editar-producto/editar-producto.component.ts
+++ src/app/editar-producto/editar-producto.component.ts
@@ -1,9 +1,39 @@
+import { ActivatedRoute } from '@angular/router';
+import { ProductoService } from '../producto.service';
+import { Producto } from './../producto';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { error } from 'console';
 
 @Component({
   selector: 'app-editar-producto',
-  templateUrl: './editar-producto.component.html'
+  templateUrl: './editar-producto.component.html',
 })
 export class EditarProductoComponent {
 
+  producto: Producto = new Producto();
+  id: number;
+
+  constructor(
+    private productoServicio: ProductoService,
+    //necesitamos este objeto para recibir el id
+    private ruta: ActivatedRoute,
+    private enrutador: Router
+  ) {}
+
+  ngOnInit() {
+    //con esta sintaxis obtenemos el id de la url en el componente cuando hacemos clic, asi tenemos el id que queremos recuperar de la base de datos
+    this.id = this.ruta.snapshot.params['id'];
+    this.productoServicio.obtenerProductoPorId(this.id).subscribe({
+      next: (datos) => {
+        this.producto = datos;
+      },
+      error: (errores: any) => console.log(errores),
+    });
+  }
+
+
+  onSubmit() {
+    //editar producto
+  }
 }
