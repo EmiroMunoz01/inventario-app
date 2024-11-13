8deea780a80f23f2fc7ad75c7aa80376bcd09f85
EmiroMunoz01 <emiro_2001@icloud.com>
2024-11-12 19:37:54 -0500

    Se ha creado la ruta en el path del archivo de routing

--- src/app/editar-producto/editar-producto.component.ts
+++ src/app/editar-producto/editar-producto.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-editar-producto',
-  templateUrl: './editar-producto.component.html',
-  styleUrl: './editar-producto.component.css'
+  templateUrl: './editar-producto.component.html'
 })
 export class EditarProductoComponent {
 
